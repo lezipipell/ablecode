dd1f84754c2fbdbcce83d119d870260d40fafef1
Umam Alfarizi <alfariziuchiha@gmail.com>
2023-02-12 02:00:26 +0800

    hmm apa yaa

--- 2-nodejs-webserver/app.js
+++ 2-nodejs-webserver/app.js
@@ -1,35 +1,36 @@
-const fs = require('fs'); //file system
+const fs = require('fs'); //file sistem
 const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    const url = req.url; //get url
+    //membuat server
+    const url = req.url; //mendapatkan url
 
     res.writeHead(200, {
         'Content-Type': 'text-html',
-    }); //give 200 ok status to page
+    }); //memberi status 200 ok pada halaman
 
     function renderHTML(path, res) {
         fs.readFile(path, (err, data) => {
             err ? res.writeHead(404).write('error: file not found') : res.write(data);
-            res.end();
+            res.end(); //mengakhiri respon server
         });
-    } // rendering html that client request
+    } // membuat fungsi mengembalikan file html dengan parameter path url dan dan respone dari server
 
     switch (url) {
         case '/about':
             renderHTML('./pages/about.html', res);
-            break; // return file 'pages/about.html, if doesnt exit will rerutn 404 status and not found message
+            break; // mengembalikan file 'pages/about.html, dan jika tidak ada akan mengembalikan status 404
         case '/contact':
             renderHTML('./pages/contact.html', res);
-            break; // return file 'pages/contact.html, if doesnt exit will rerutn 404 status and not found message
+            break; // mengembalikan file 'pages/contact.html, dan jika tidak ada akan mengembalikan status 404
 
         default:
             renderHTML('./pages/index.html', res);
-            break; // return file 'pages/index.html, if doesnt exit will rerutn 404 status and not found message
-    } //simple get request routing
+            break; // mengembalikan file 'pages/index.html, dan jika tidak ada akan mengembalikan status 404
+    } // route sederhana dengan percabangan switch case
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}...`); //message in console while server is running
+    console.log(`Server is running on port ${port}...`); // pesan di konsol saad server berjalan
 });
--- ablecode/server-file.js
+++
@@ -1,8 +0,0 @@
-var http = require('http'),
-parse = require('url').parse,
-join = require('join'),
-fs = require('fs'),
-root = join(__dirname, 'www'),
-PORT = 3400
-
-server = http.createServer(function(req, res))
